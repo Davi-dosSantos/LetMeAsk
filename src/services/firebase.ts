4eaa234d3dcca7f2f8aa50929d9c24bcf20cf6ed
Davi Costa <daviita1@hotmail.com>
2021-06-21 19:29:07 -0300

    att firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -5,13 +5,13 @@ import 'firebase/database';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCOQ8ZLeqPG6rzlxDt5JyYdkCS29_wGA9A",
-    authDomain: "letmeask-4781b.firebaseapp.com",
-    databaseURL: "https://letmeask-4781b-default-rtdb.firebaseio.com",
-    projectId: "letmeask-4781b",
-    storageBucket: "letmeask-4781b.appspot.com",
-    messagingSenderId: "593576424859",
-    appId: "1:593576424859:web:2c9bd6d923e62a332edd3e"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
