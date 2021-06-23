84d2a877a18bcf0c175fa4e9dc6866d74101ac12
Davi Costa <daviita1@hotmail.com>
2021-06-23 11:19:19 -0300

    hook de autenticação criado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,5 @@
 import { useHistory } from 'react-router';
 
-import { auth, firebase } from '../services/firebase';
-
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
@@ -10,12 +8,11 @@ import { Button } from '../components/Button';
 
 
 import '../styles/auth.scss'
-import { useContext } from 'react';
-import { AuthContext } from '../App';
+import { useAuth } from '../hooks/useAuth';
 
 export function Home() {
     const history = useHistory();
-    const { user, signInWithGoogle } = useContext(AuthContext)
+    const { user, signInWithGoogle } = useAuth();
 
     async function handleCreateRoom() {
         if (!user) {
